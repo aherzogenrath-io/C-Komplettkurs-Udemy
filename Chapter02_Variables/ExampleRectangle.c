a4253288e7bbac7b9d73d74ac2306bc79d448b84
aherzogenrath-io <aherzogenrath@infonline.de>
2024-09-11 12:11:32 +0200

    rectangle excersize

--- Chapter02_Variables/ExampleRectangle.c
+++ Chapter02_Variables/ExampleRectangle.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
     // Rectangle Example
     // 1.) Read the values for a, b from the user
     // 2.) Compute the perimeter and area of the rectangle
     // 3.) Print the computed values to the console
 
-    float a;
-    float b;
+    float side_a;
+    float side_b;
+    float area;
+    float perimiter;
 
-    printf("Enter a value for a: ");
-    scanf("%f", &a);
-    printf("Enter a value for b: ");
-    scanf("%f", &b);
+    printf("Enter side a: ");
+    scanf("%f", &side_a);
 
-    float perimeter = 2.0F * a + 2.0F * b;
-    float area = a * b;
+    printf("\nEnter side b: ");
+    scanf("%f", &side_b);
 
-    printf("Perimeter: %f\n", perimeter);
-    printf("Area: %f\n", area);
+
+    area = side_a * side_b;
+    perimiter = (2.0F * side_a) + (2.0F * side_b);
+
+    printf("\nThe area of the rectangle: %f\n", area);
+    printf("\nThe perimiter of the rectangle: %f\n", perimiter);
+
 
     return 0;
 }
